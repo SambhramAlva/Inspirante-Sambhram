7f5d67d74f7588418f2c89ccb0a8311775b4e10d
SambhramAlva <sambhramk2006@gmail.com>
2026-06-02 21:47:36 +0530

    database structures updated

---
+++ backend/models/Event.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const eventSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+
+  date: {
+    type: Date,
+    required: true
+  },
+
+  venue: {
+    type: String,
+    required: true
+  },
+
+  capacity: {
+    type: Number,
+    required: true
+  }
+});
+
+module.exports = mongoose.model("Event", eventSchema);
