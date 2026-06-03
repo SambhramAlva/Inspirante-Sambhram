ad9819e44a171cea2c0cb612b0e50c3d556c0396
SambhramAlva <sambhramk2006@gmail.com>
2026-06-03 14:24:58 +0530

    logout button

--- app/src/components/AdminSidebar.jsx
+++ app/src/components/AdminSidebar.jsx
@@ -1,6 +1,17 @@
 import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 function AdminSidebar() {
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+
+    localStorage.removeItem("token");
+    localStorage.removeItem("role");
+    localStorage.removeItem("userId");
+
+    navigate("/");
+  };
   return (
     <aside className="sidebar">
         <h2>Admin Panel</h2>
@@ -16,9 +27,12 @@ function AdminSidebar() {
             View Events
           </Link>
 
-          <Link to="/">
-            Logout
-          </Link>
+          <button
+  className="logout-btn"
+  onClick={handleLogout}
+>
+  Logout
+</button>
         </nav>
       </aside>
   );
